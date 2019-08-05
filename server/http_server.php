97cde168be52299e136ef085862f0f7d59bd7bfb
shuyh <nelson.shu.chn@gmail.com>
2019-08-05 18:04:37 +0800

    swoole 完美支持 thinkphp

--- server/http_server.php
+++ server/http_server.php
@@ -9,10 +9,49 @@
 $http = new Swoole\Http\Server("0.0.0.0", 9501);
 
 $http->set([
-    'document_root' => '/data/wwwroot/docment/swoole_live/public/static', // v4.4.0以下版本, 此处必须为绝对路径
-    'enable_static_handler' => true,
+    'document_root'          => '/data/wwwroot/docment/swoole_live/public/static', // v4.4.0以下版本, 此处必须为绝对路径
+    'enable_static_handler'  => true,
+    'worker_num'             => 5
 ]);
+
+$http->on('WorkerStart', function (swoole_server $server, $worker_id){
+    // 定义应用目录
+    define('APP_PATH', __DIR__ . '/../application/');
+    // ThinkPHP 引导文件
+    // 加载基础文件
+    require __DIR__ . '/../thinkphp/base.php';
+
+});
+
 $http->on('request', function ($request, $response) {
-    $response->end("<h1>Hello Swoole. #".rand(1000, 9999)."</h1>");
+    if(isset($request->server)){
+        foreach ($request->server as $k => $v){
+            $_SERVER[strtoupper($k)] = $v;
+        }
+    }
+    if(isset($request->header)){
+        foreach ($request->header as $k => $v){
+            $_SERVER[strtoupper($k)] = $v;
+        }
+    }
+    if(isset($request->get)){
+        foreach ($request->get as $k => $v){
+            $_GET[$k] = $v;
+        }
+    }
+    if(isset($request->post)){
+        foreach ($request->post as $k => $v){
+            $_POST[$k] = $v;
+        }
+    }
+    //开启缓冲区
+    ob_start();
+    // 执行应用并响应
+    think\Container::get('app', [defined('APP_PATH') ? APP_PATH : ''])
+        ->run()
+        ->send();
+    $res = ob_get_contents();
+    ob_end_clean();
+    $response->end($res);
 });
 $http->start();
